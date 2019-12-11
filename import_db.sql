eaed82ee816be9303d002ec54325148df31729b7
J Resilient <j.resilient.1@gmail.com>
2019-12-11 12:57:10 -0500

    Fix typo

--- import_db.sql
+++ import_db.sql
@@ -43,6 +43,7 @@ VALUES
 
 CREATE TABLE question_follows
 (
+  id INTEGER PRIMARY KEY,
   question_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
 
@@ -86,6 +87,7 @@ VALUES
 
 CREATE TABLE question_likes
 (
+  id INTEGER PRIMARY KEY,
   question_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
 
