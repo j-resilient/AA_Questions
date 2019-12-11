4f82c0a6cc3b3681ea6d335250757ae6e07c28e3
J Resilient <j.resilient.1@gmail.com>
2019-12-11 14:28:54 -0500

    Add ::find_by_user_id and ::find_by_question_id

--- reply.rb
+++ reply.rb
@@ -15,6 +15,30 @@ class Reply
         reply.length > 0 ? Reply.new(reply.first) : nil
     end
 
+    def self.find_by_user_id(user_id)
+        reply = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+            SELECT
+               *
+            FROM
+               replies
+            WHERE
+               user_id = ?
+        SQL
+        reply.length > 0 ? Reply.new(reply.first) : nil
+    end
+
+    def self.find_by_question_id(question_id)
+        reply = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+            SELECT
+              *
+            FROM
+              replies
+            WHERE
+              question_id = ?
+        SQL
+        reply.length > 0 ? reply.map { |r| Reply.new(r) } : nil
+    end
+
     def initialize(options)
         @id = options['id']
         @question_id = options['question_id']
--- spec/reply_spec.rb
+++ spec/reply_spec.rb
@@ -11,4 +11,33 @@ describe 'Reply' do
             expect(Reply.find_by_id(1)).to be_an_instance_of(Reply)
         end
     end
+
+    describe "::find_by_user_id" do
+        it 'returns nil if user_id has written no replies' do
+            expect(Reply.find_by_user_id(10)).to eq(nil)
+        end
+        it 'returns a reply object' do
+            expect(Reply.find_by_user_id(1)).to be_an_instance_of(Reply)
+        end
+        it 'returns the correct reply' do
+            reply = Reply.find_by_user_id(1)
+            expect(reply.id).to eq(1)
+        end
+    end
+
+    describe "::find_by_question_id" do
+        it 'returns nil if question has no replies' do
+            expect(Reply.find_by_question_id(3)).to eq(nil)
+        end
+        it 'returns a reply objects' do
+            replies = Reply.find_by_question_id(2)
+            replies.each do |reply|
+                expect(reply).to be_an_instance_of(Reply)
+            end
+        end
+        it 'returns all replies' do
+            replies = Reply.find_by_question_id(2)
+            expect(replies.length).to eq(3)
+        end
+    end
 end
