214bf7d329c3279d8f215e5fbcf561a50fcdb0cf
J Resilient <j.resilient.1@gmail.com>
2019-12-11 12:04:35 -0500

    Change order based on app-academy solution

--- import_db.sql
+++ import_db.sql
@@ -1,95 +1,105 @@
-PRAGMA foreign_keys = ON;
-
-DROP TABLE IF EXISTS question_likes;
-
-CREATE TABLE question_likes (
-    question_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
-
-    FOREIGN KEY (question_id) REFERENCES questions(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
-
-DROP TABLE IF EXISTS replies;
-
-CREATE TABLE replies (
-    id INTEGER PRIMARY KEY,
-    question_id INTEGER NOT NULL,
-    parent_id INTEGER,
-    user_id INTEGER NOT NULL,
-    body TEXT NOT NULL,
-
-    FOREIGN KEY (question_id) REFERENCES questions(id),
-    FOREIGN KEY (parent_id) REFERENCES replies(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
-
 DROP TABLE IF EXISTS question_follows;
-
-CREATE TABLE question_follows
-(
-    question_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
-
-    FOREIGN KEY (question_id) REFERENCES questions(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
-
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
 DROP TABLE IF EXISTS questions;
-
-CREATE TABLE questions
-(
-    id INTEGER PRIMARY KEY,
-    title TEXT,
-    body TEXT,
-    author_id INTEGER NOT NULL,
-
-    FOREIGN KEY (author_id) REFERENCES users(id)
-);
-
 DROP TABLE IF EXISTS users;
+
+PRAGMA foreign_keys = ON;
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
-  fname TEXT,
-  lname TEXT NOT NULL
+  fname VARCHAR(255),
+  lname VARCHAR(255) NOT NULL
 );
 
 INSERT INTO
   users (fname, lname)
 VALUES
-  ('Tony', 'Stark'),
-  ('Pepper', 'Potts'),
-  ('James', 'Rhodes');
+  ('Tony', 'Stark'), ('Pepper', 'Potts'), ('James', 'Rhodes');
+
+
+CREATE TABLE questions
+(
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  body TEXT,
+  author_id INTEGER NOT NULL,
+
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
 
 INSERT INTO
   questions
-    (title, body, author_id)
+  (title, body, author_id)
 VALUES
-    ('Social Security Number', 'What is your social security number?', (
+  ('Social Security Number', 'What is your social security number?', (
       SELECT
-            id
-        FROM
-            users
-        WHERE
+      id
+    FROM
+      users
+    WHERE
         lname LIKE 'Potts'
-  ));
+    ));
+
+CREATE TABLE question_follows
+(
+  question_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
 INSERT INTO
-  question_follows (question_id, user_id)
+  question_follows
+  (question_id, user_id)
 VALUES
-  ((SELECT id FROM questions WHERE title = 'Social Security Number'),
-  (SELECT id FROM users WHERE lname = 'Rhodes'));
+  ((SELECT id
+    FROM questions
+    WHERE title = 'Social Security Number'),
+    (SELECT id
+    FROM users
+    WHERE lname = 'Rhodes'));
+
+CREATE TABLE replies
+(
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER NOT NULL,
+  parent_id INTEGER,
+  user_id INTEGER NOT NULL,
+  body TEXT NOT NULL,
+
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (parent_id) REFERENCES replies(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
 INSERT INTO
-  replies (question_id, user_id, body)
+  replies
+  (question_id, user_id, body)
 VALUES
-  (1, 
-  (SELECT id FROM users WHERE lname = 'Stark' AND fname = 'Tony'),
-  '5? There is definitely a 5 in there.');
+  (1,
+    (SELECT id
+    FROM users
+    WHERE lname = 'Stark' AND fname = 'Tony'),
+    '5? There is definitely a 5 in there.');
+
+CREATE TABLE question_likes
+(
+  question_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
 INSERT INTO
-  question_likes (question_id, user_id)
+  question_likes
+  (question_id, user_id)
 VALUES
-  ((SELECT id FROM questions WHERE title = 'Social Security Number'),
-  (SELECT id FROM users WHERE lname = 'Rhodes'));
+  ((SELECT id
+    FROM questions
+    WHERE title = 'Social Security Number'),
+    (SELECT id
+    FROM users
+    WHERE lname = 'Rhodes'));
