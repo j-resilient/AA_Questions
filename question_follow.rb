af5539707308334558e43929bf5a33b07d09098b
J Resilient <j.resilient.1@gmail.com>
2019-12-12 11:52:09 -0500

    Add ::followers_for_question_id

--- question_follow.rb
+++ question_follow.rb
@@ -13,6 +13,20 @@ class QuestionFollow
         follow.length > 0 ? QuestionFollow.new(follow.first) : nil
     end
 
+    def self.followers_for_question_id(question_id)
+        followers = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+            SELECT
+              *
+            FROM
+              users
+            JOIN
+              question_follows ON users.id = question_follows.user_id
+            WHERE
+              question_follows.question_id = ?
+        SQL
+        followers.length > 0 ? followers.map { |f| User.new(f) } : nil
+    end
+
     def initialize(options)
         @id = options['id']
         @question_id = options['question_id']
--- spec/question_follow_spec.rb
+++ spec/question_follow_spec.rb
@@ -11,4 +11,21 @@ describe 'QuestionFollow' do
             expect(QuestionFollow.find_by_id(1)).to be_an_instance_of(QuestionFollow)
         end
     end
+
+    describe "::followers_for_question_id" do
+        subject(:followers) { QuestionFollow.followers_for_question_id(1) }
+        it "returns an array of User objects" do
+            expect(followers).to be_an_instance_of(Array)
+            followers.each do |follower|
+                expect(follower).to be_an_instance_of(User)
+            end
+        end
+        let(:q1_followers) { ['Potts', 'Stark'] }
+        it "returns the correct followers for the given question id" do
+            expect(followers.length).to eq(2)
+            followers.each_with_index do |follower, idx|
+                expect(follower.lname).to eq(q1_followers[idx])
+            end
+        end
+    end
 end
